f144212be47ce66deca124b49bf2ce596351f210
zb3310076 <769787258@qq.com>
2018-07-03 20:27:19 +0800

    zb2

---
+++ src/main/java/com/gitdemo/com/jk/bean/Qian.java
@@ -0,0 +1,50 @@
+package com.gitdemo.com.jk.bean;
+
+import java.util.Date;
+
+/**
+ * @program: demo
+ * @description: ${description}
+ * @author: zb
+ * @create: 2018-07-03 20:24
+ **/
+public class Qian {
+
+    private Integer id;
+    private String name;
+    private Date date;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Qian{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
